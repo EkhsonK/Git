2ddee80874bb9ecb9cb4d6bb95a72f53578378bb
student <korcaevehson@gmail.com>
2024-03-13 09:50:17 +0300

    2

--- 5.py
+++ 5.py
@@ -8,6 +8,11 @@ d = {alf[i]:i+1 for i in range(66)}
 # функция задаёт строке уникальный хэш
 
 def hash(s):
+    s = s.replace(' ','')
     p = 67
     m = (10**9)+9
     hash_value = 0
+    for c in s:
+        hash_value += d[c] *p **i
+        i+=1
+    return hash_value % m
